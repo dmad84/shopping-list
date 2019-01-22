dcf5de03bf520742470126d39b74f26ad902b7a4
MADHESWARAN Deepa <deepa.madheswaran@soprasteria.com>
2019-01-22 15:24:46 +0000

    Renamed AddItem to ShoppingList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AddItem from './Components/AddItem';
+import ShoppingList from './Components/ShoppingList';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <AddItem />
+        <ShoppingList />
       </div>
     );
   }

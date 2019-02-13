7d1c55cdb5374b4a6112637bb50e52c28816ae5a
MADHESWARAN Deepa <deepa.madheswaran@soprasteria.com>
2019-02-13 16:17:10 +0000

    Added in errors component

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import firebase from '../../firebase';
 import PropTypes from 'prop-types';
 import Categories from '../Categories/Categories';
+import Errors from '../Errors/Errors';
 import './AddItem.css';
 
 export default class AddItem extends Component {
@@ -10,31 +11,27 @@ export default class AddItem extends Component {
     this.state = {
       text: '',
       selectedCategory: '',
-      errors: ''
+      errors: []
     };
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSelectChange = this.handleSelectChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.validate = this.validate.bind(this);
   }
   render() {
     const categories = this.props.categories;
     return (
       <form onSubmit={this.handleSubmit} className="my-3 col-lg-8">
         <div className="form-row">
+          {
+            this.state.errors.length > 0 && <Errors errors={this.state.errors} />
+          }
           <div className="form-group col-md-6">
             <label className="sr-only">Item</label>
             <input placeholder="enter Item" onChange={this.handleTextChange} value={this.state.text} className="form-control">
             </input>
-            {
-              this.state.errors.text &&
-              <span> {this.state.errors.text}</span>
-            }
           </div>
           <Categories items={categories} update={this.handleSelectChange} />
-          {
-            this.state.errors.category &&
-            <span> {this.state.errors.category}</span>
-          }
           <div className="form-group col-md-2">
             <button type="submit" className="btn btn-primary">add</button>
           </div>
@@ -46,39 +43,38 @@ export default class AddItem extends Component {
     this.setState({ text: e.target.value });
     if (e.target.value.length > 0) {
       this.setState(state => ({
-        errors: {
-          text: ''
-        }
+        errors: {}
       }));
     }
   }
   handleSelectChange(id) {
+    this.setState(state => ({
+      errors: {}
+    }));
     this.setState({ selectedCategory: id });
+  }
+  validate() {
+    let formErrors = [];
+    let output = true;
+
+    if (!this.state.text.length) {
+      formErrors.push('Please enter an item')
+      output = false;
+    }
+    if (!this.state.selectedCategory.length) {
+      formErrors.push('Please select a category')
+      output = false;
+    }
+    this.setState(state => ({
+      errors: formErrors
+    }));
+    return output;
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    const itemsRef = firebase.database().ref('shopping-items').child('categories');
-
-    if (!this.state.text.length || !this.state.selectedCategory.length) {
-      if (!this.state.text.length) {
-        this.setState(state => ({
-          errors: {
-            text: 'Please enter an item'
-          }
-        }));
-      }
-      if (!this.state.selectedCategory.length) {
-        this.setState(state => ({
-          errors: {
-            category: 'Please select a category'
-          }
-        }));
-      }
-
-    }
-    else {
-
+    if (this.validate()) {
+      const itemsRef = firebase.database().ref('shopping-items').child('categories');
       const newItem = {
         name: this.state.text
       };
